fbf9a27ae743f8b547ace8d74871f0fb78aefb57
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-30 14:35:49 +0300

    added content to p

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+
+const paragraph = document.querySelector( 'p' )
+//The DOMContentLoaded event is the browser's built-in way to indicate when a page's html is loaded into the DOM.
+document.addEventListener('DOMContentLoaded', function(){
+    let p = document.getElementById('text');
+    p.textContent = 'This is really cool!'
+})
